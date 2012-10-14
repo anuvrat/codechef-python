8d91cfc2b1282188ebd2167e58d6d1fde8ae5226
Anuvrat Singh <singh.anuvrat@gmail.com>
2012-10-15 01:02:44 +0530

    Solution to CMB01

---
+++ src/practice/peer/CMB01.py
@@ -0,0 +1,15 @@
+import sys
+
+def reverse( num_str ):
+    n_rev = 0
+    n = int( num_str )
+    while n > 0: n_rev, n = n_rev * 10 + n % 10, n / 10
+
+    return n_rev
+
+def main():
+    for i in xrange( int( sys.stdin.readline() ) ):
+        a , b = map( reverse, sys.stdin.readline().split() )
+        print reverse( a + b )
+
+main()
